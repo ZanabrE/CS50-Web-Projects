e5e8ac1bddfc601c9291ec3f432c5ece63558960
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-10-13 12:02:56 -0400

    modified files

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,6 +4,7 @@ from . import util
 
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
