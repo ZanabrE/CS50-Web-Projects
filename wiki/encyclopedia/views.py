bef13b26a1596b17bcead3f2177012d83516413e
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-10-13 12:29:05 -0400

    modified files

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from markdown2 import markdown
+from markdown2 import Markdown
 import markdown 
 
 from . import util
 
 
 def convert_md_to_html(title):
-    content: util.get_entry(title)
-    markdowner = markdown.Mardown()
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
     if content == None:
         return None
     else:
