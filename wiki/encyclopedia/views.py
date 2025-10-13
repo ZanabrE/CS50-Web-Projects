5af831983ae02cf9696718d02efc2f42a2e238b1
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-10-13 12:13:37 -0400

    modified files

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from markdown2 import markdown
+import markdown 
 
 from . import util
+
+
+def convert_md_to_html(title):
+    content: util.get_entry(title)
+    markdowner = markdown.Mardown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):
@@ -8,4 +19,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    html_page = convert_md_to_html(title)
+    if html_page == None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_page
+        })
 
