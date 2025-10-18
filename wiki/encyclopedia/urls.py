df86df61e14098f8c05288969eb066a57a7aa6c2
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-10-18 14:06:00 -0400

    modified: wiki/encyclopedia/urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("edit", views.edit, name="edit"),
+    path("search/", views.search, name="search"),
+    path("new/", views.new_page, name="new_page"),
+    path("edit/", views.edit, name="edit"),
+    path("save_entry/", views.save_entry, name="save_entry"),
+    path("random/", views.random_page, name="random_page")
 ]
+
