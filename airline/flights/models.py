601d0ec43ec477bf2ef7425ff314e4f331d0260e
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-10-28 21:03:54 -0400

    deleted and mofidied a file

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,12 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    origin = moelds.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
     duration = models.IntegerField()
-    
-    def __str__(self):
-        return f"{self.id}: {self.origin} to {self.destination}"
-
-
         
