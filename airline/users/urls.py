382ced6dd0bb6c317ecda702071b671a607d335c
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-11-02 12:33:21 -0500

    new files added and modified

--- airline/users/urls.py
+++ airline/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
+    path('login', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
     
 ]
