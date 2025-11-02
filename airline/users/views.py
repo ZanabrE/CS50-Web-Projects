02fe5efbc03dc642c2b077cc3846e54070f81ce0
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-11-02 11:44:11 -0500

    new file added and mnodified

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html")
+    
+'''
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+'''
