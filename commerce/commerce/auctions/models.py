802eaa66e929246c00b09c3e4fb8bdcc08066fb5
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-11-08 13:24:24 -0500

    modified files and added new files

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} - Starting at ${self.starting_bid}"
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"${self.amount} by {self.bidder.username}"
+    
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Comment by {self.commenter.username} on {self.listing.title}"
