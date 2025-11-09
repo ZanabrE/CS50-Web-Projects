f66afd662f6beaf1c31a7b8c7c7d32f8edefebe7
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2025-11-09 13:34:59 -0500

    modified files and added files

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -5,10 +5,20 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    categoryName = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.categoryName
+
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    imageURL = models.URLField(blank=True, null=True)
+    price = models.FloatField(default=0)
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
 
     def __str__(self):
         return f"{self.title} - Starting at ${self.starting_bid}"
