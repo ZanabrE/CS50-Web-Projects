9d89211744fcc6cf80f821db1d6c489ad3eaaed3
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2026-03-22 12:22:02 -0400

    modified: counter.js

--- PracticeJS/counter.js
+++ PracticeJS/counter.js
@@ -1,17 +1,22 @@
-let count = 0;
+if (!localStorage.getItem("count")){
+    localStorage.setItem("count", 0);
+}
 
 function myFunction() {
+    let count = localStorage.getItem("count");
     count++;
     //alert("Count: " + count);
     document.getElementById("counter").innerHTML = "Count: " + count;
+    localStorage.setItem("count", count);
 
     // Using if stament to check if the count is a multiple of 10 and display an alert message
-    if (count % 10 === 0) {
+    /*if (count % 10 === 0) {
         alert(`Count is a multiple of 10: ${count}`);
-    }
+    }*/
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     //document.querySelector("button").addEventListener("click", myFunction); // one way to add event listener to the button
+    document.querySelector("#counter").innerHTML = localStorage.getItem("count"); // display the count value when the page is loaded
     document.querySelector("button").onclick = myFunction; // another way to add event listener to the button
 });
