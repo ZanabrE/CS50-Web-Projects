008010328912fb15ff527fd5d873b3212714126d
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2026-03-23 20:16:22 -0400

    modified files

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Using POST method to send email data to the server and then load the sent mailbox after the email is sent successfully
+  document.querySelector('#compose-form').onsubmit = function() {
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent');
+    });
+
+    return false;
+
+  };
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -23,11 +44,43 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
+  // Using GET method to fetch the emails of the selected mailbox from the server and then display them in the mailbox view
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // fetch the emails of the selected mailbox from the server
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Loop through emails and create a row for each email
+    emails.forEach(email => {
+      const email_element = document.createElement('div');
+      email_element.className = 'email'; // Add a class for CSS styling
+
+      //Set background color based on read status
+      email_element.style.backgroundColor = email.read ? '#e5e5e5' : 'white';
+      email_element.style.border = '1px solid black';
+      email_element.style.padding = '10px';
+      email_element.style.margin = '5px 0';
+
+    // Add email content to the email element
+      email_element.innerHTML = `
+        <strong>${email.sender}</strong>
+        <span>${email.subject}</span>
+        <span style="float:right; color:gray;">${email.timestamp}</span>
+        `;
+
+      // Add click event to view the email (for the next part of the project)
+      email_element.addEventListener('click', () => {
+        console.log('This element has been clicked!');
+      });
+
+      document.querySelector('#emails-view').appendChild(email_element);
+    });
+
+  });
+
 }
