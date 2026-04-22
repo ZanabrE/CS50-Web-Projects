8e03a8d2a83c2186430e2612f9926fb3aed94ace
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2026-04-22 19:17:22 -0400

    modified: assert.py

--- assert.py
+++ assert.py
@@ -1,4 +1,13 @@
-def square(x):
+# This code is good sample.
+"""def square(x):
     return x * x
 
-assert square(10) == 100
+assert square(10) == 100"""
+
+#print(square(10) == 101
+
+# Using Assert with a bug 
+def square(x):
+    return x + x
+
+assert square(10) == 100  # This will fail
